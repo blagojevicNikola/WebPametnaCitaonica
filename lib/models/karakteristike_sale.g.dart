c809136262b009e65045990fe270d4815faed625
Skobic <milosskobic@hotmail.com>
2022-04-04 23:27:17 +0200

    comit obavezni

--- lib/models/karakteristike_sale.g.dart
+++ lib/models/karakteristike_sale.g.dart
@@ -10,10 +10,12 @@ KarakteristikeSale _$KarakteristikeSaleFromJson(Map<String, dynamic> json) =>
     KarakteristikeSale(
       karakteristikaId: json['karakteristikaId'] as int?,
       detalji: json['detalji'] as String?,
+      naziv: json['naziv'] as String?,
     );
 
 Map<String, dynamic> _$KarakteristikeSaleToJson(KarakteristikeSale instance) =>
     <String, dynamic>{
       'karakteristikaId': instance.karakteristikaId,
+      'naziv': instance.naziv,
       'detalji': instance.detalji,
     };

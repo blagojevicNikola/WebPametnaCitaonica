413fa4815aab5a86ef1e852f41db68f849eb4445
blagojevicNikola <nikolablagojevic848@gmail.com>
2022-04-04 23:21:07 +0200

    za milosinu

---
+++ lib/api/auth_service.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:web_aplikacija/api/dio_client.dart';
+
+import '../models/web_login.dart';
+
+class AuthService {
+  final DioClient _dioClient = DioClient();
+
+  Future<Response?> postLogin(String username, String password) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    Response? temp;
+    try {
+      temp = await _dioClient.dio.post('/prijava',
+          data: {"korisnickoIme": username, "lozinka": password});
+
+      // Prints the raw data returned by the server
+      //print('User Info: ${userData.data}');
+      if (temp.statusCode == 200) {
+        WebLogin korisnik = WebLogin.fromJson(temp.data);
+        prefs.setString('accessToken', korisnik.accessToken);
+        prefs.setString('refreshToken', korisnik.refreshToken);
+      }
+      // Parsing the raw JSON data to the User class
+    } catch (error, stacktrace) {
+      throw Exception("Exception occured: $error stackTrace: $stacktrace");
+    }
+    return temp;
+  }
+}
--- lib/api/individualne_sale_service.dart
+++ lib/api/individualne_sale_service.dart
@@ -1,15 +1,16 @@
 import 'package:dio/dio.dart';
+import 'package:web_aplikacija/api/dio_client.dart';
 import 'package:web_aplikacija/models/individualna_sala.dart';
 
 class IndividualneSaleService {
-  final Dio _dio = Dio();
+  final DioClient _dioClient = DioClient();
 
   final _baseUrl = 'http://localhost:8080/api/v1';
 
   Future<List<IndividualnaSala>> getIndividualneSale(String citaonicaId) async {
     // Perform GET request to the endpoint "/users/<id>"
     try {
-      Response saleData = await _dio.get(
+      Response saleData = await _dioClient.dio.get(
           _baseUrl + '/citaonice/${citaonicaId.toString()}/individualne-sale');
 
       // Prints the raw data returned by the server
@@ -31,7 +32,7 @@ class IndividualneSaleService {
     IndividualnaSala? retrievedIndividualnaSala;
 
     try {
-      Response response = await _dio.post(
+      Response response = await _dioClient.dio.post(
         _baseUrl + '/citaonice/${citaonicaId}/individualne-sale',
         data: sala.toJson(),
       );
@@ -48,7 +49,7 @@ class IndividualneSaleService {
 
   Future<String> deleteIndividualnaSala(
       {required String citaonicaId, required String individualnaSalaId}) async {
-    Response response = await _dio.delete(
+    Response response = await _dioClient.dio.delete(
       _baseUrl +
           '/citaonice/$citaonicaId/individualne-sale/$individualnaSalaId',
     );
@@ -66,7 +67,7 @@ class IndividualneSaleService {
     IndividualnaSala? retrievedIndividualnaSala;
 
     try {
-      Response response = await _dio.put(
+      Response response = await _dioClient.dio.put(
         _baseUrl +
             '/citaonice/$citaonicaId/individualne-sale/${individualnaSalaData.id}',
         data: individualnaSalaData.toJson(),
